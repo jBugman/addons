7451e8e1a336f15d5575bed06712ef0a57e2f5c4
Sergey Parshukov <codedby@bugman.me>
2019-08-16 23:50:30 +0300

    CLI for listing

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,25 @@
+use structopt::StructOpt;
+
 mod addon;
 mod error;
 
-fn main() -> error::Result<()> {
-    let list = addon::list_installed(addon::Dir::Default)?;
-    let a = list.first().unwrap();
-    println!("{:#?}", a);
-    let toc = a.get_toc()?;
-    println!("{:#?}", toc);
+#[derive(Debug, StructOpt)]
+#[structopt(name = "addons", about = "Simple addon manager for WoW")]
+enum CLI {
+    #[structopt(name = "list", about = "Lists installed addons")]
+    ListInstalled,
+}
+
+fn main() -> Result<(), error::Error> {
+    let app = CLI::from_args();
+    match &app {
+        CLI::ListInstalled => {
+            let mut list = addon::list_installed(addon::Dir::Default)?;
+            list.sort_unstable_by(|a, b| a.name.cmp(&b.name));
+            for a in list {
+                println!("{}", a);
+            }
+        }
+    }
     Ok(())
 }
